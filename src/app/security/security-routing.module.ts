c29d1ed5e6fddae2d186559426f71cbf4dc80d13
ctucnoc <ctc.tucno@gmail.com>
2023-04-07 18:52:25 -0500

    primer commit

---
+++ src/app/security/security-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SecurityLayoutComponent } from './layout/security-layout/security-layout.component';
+import { UserComponent } from './pages/user/user.component';
+import { RoleComponent } from './pages/role/role.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: SecurityLayoutComponent,
+    children: [
+      { path: 'user', component: UserComponent },
+      { path: 'role', component: RoleComponent },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class SecurityRoutingModule {}
